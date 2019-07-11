ffe24c625d0006385372080695afd304bd893b2a
georgbischoff <51708474+georgbischoff@users.noreply.github.com>
2019-07-11 10:07:18 +0200

    Update karma.conf.js
    removed obsolete reference to qunit

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function (config) {
   config.set({
-    frameworks: ["ui5","qunit"],
+    frameworks: ["ui5"],
     ui5: {
       url: "https://openui5.hana.ondemand.com"
     },	    
