9eba96f95ccd9d6a5819974cf142c6570d96118d
Shahzeb khan <shahzeb.khan@sap.com>
2020-03-11 02:30:03 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -2,10 +2,11 @@ module.exports = function (config) {
   config.set({
     frameworks: ["ui5"],
     ui5: {
-      url: "https://openui5.hana.ondemand.com"
+      url: "https://sapui5.hana.ondemand.com",
+      mode: "html",
+      testpage: "webapp/test/testsuite.qunit.html"
     },
-    
-    browsers: ["ChromeCustom"],   
+    browsers: ["ChromeHeadless"],   
     customLaunchers: {
       ChromeCustom: {
         base: 'Chrome',
@@ -15,50 +16,18 @@ module.exports = function (config) {
         base: 'ChromeHeadless',
         flags: ['--window-size=1600,900']
       }
-    },
-
+    },  
+    singleRun: false,
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTimeout: 60000,
+    autoWatch: true,
     browserConsoleLogOptions: {
       level: 'warn'
     },
+    logLevel: config.LOG_WARN,
 
     preprocessors: {
-      '**/webapp/!(test|localService)/**/*.js': ['coverage']
-    },
-    coverageReporter: {
-      includeAllSources: true,
-      reporters: [
-        {
-          type: 'html',
-          dir: 'target/coverage',
-          subdir: '.'
-        },
-        {
-          type: 'text'
-        }
-      ]
-    },
-    coverageReporter: {
-      dir: 'target/coverage',
-      subdir: '.'
-      // Would output the results into: .'/coverage/'
-    },
-    junitReporter: {
-      outputDir: "./target/junit",
-      outputFile: "TEST-qunit.xml",
-      suite: "",
-      useBrowserName: true
-    },
-    htmlReporter: {
-      outputFile: './target/html/JUnit.html',
-
-      // Optional
-      pageTitle: 'Test Results',
-      subPageTitle: 'Detailed test results for OPA 5',
-      groupSuites: true,
-      useCompactStyle: true,
-      useLegacyStyle: true,
-      showOnlyFailed: false
-    },
-    reporters: ['progress', 'coverage', 'junit', 'html']
+      '**/webapp/!(resources|maven|test-resources|test|localService|localService2)/**/*.js': ['coverage']
+    }
   });
 };
